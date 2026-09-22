27b61ecbdb23688535c44753b6b2267e00758689
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2026-09-22 15:47:48 +0200

    Chore: add @nestjs/swagger for API documentation and configure Swagger in main application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 function requireEnv(name: string): string {
   const value = process.env[name];
@@ -16,6 +17,16 @@ async function bootstrap() {
   requireEnv('DATABASE_URL');
 
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Kanban Board API')
+    .setDescription('The Kanban Board API description')
+    .setVersion('1.0')
+    .addTag('kanban')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
